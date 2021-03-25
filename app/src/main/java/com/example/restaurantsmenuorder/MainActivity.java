408d16afdfef011b102727411a3de560f38ea519
Hussam Alshayeb <hos_1417@hotmail.com>
2021-03-25 05:12:01 +0300

    ui design

--- app/src/main/java/com/example/restaurantsmenuorder/MainActivity.java
+++ app/src/main/java/com/example/restaurantsmenuorder/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.restaurantsmenuorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+
     }
 }
